c987b96ca201c578655d00e6300163c1c7df05f0
hmlu36 <hmlu36@gmail.com>
2020-12-17 17:43:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <RegiestForm v-bind:profile="profile" />
+</template>
+
+<script>
+import RegiestForm from './components/RegiestForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RegiestForm,
+  },
+  data() {
+    return {
+      msg: '',
+      profile: {},
+    };
+  },
+  beforeCreate() {},
+  methods: {},
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Noto+Sans+TC');
+* {
+  font-family: 'Noto Sans TC';
+}
+</style>
