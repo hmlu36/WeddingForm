606f7807518d2578739f48e873c619bf34727d73
hmlu36 <hmlu36@gmail.com>
2020-12-21 11:59:55 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ const path = require('path')
 // vue.config.js
 module.exports = {
   base: '/WeddingForm/', //發佈對應路徑
-  alias: {
-    // 键必须以斜线开始和结束
-    '/@/': path.resolve(__dirname, './src')
-    // '/@components/': path.resolve(__dirname, './src/components')
-  },
+  assetsDir: "",
 };
