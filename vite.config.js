9c15405e6460bc4fbb2ec38df9e33bc5c1369e43
hmlu36 <hmlu36@gmail.com>
2020-12-21 11:22:49 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // vue.config.js
 
 module.exports = {
-  base: process.env.NODE_ENV === 'production' ? '/WeddingForm/' : '/', //發佈對應路徑
+  indexPath: process.env.NODE_ENV === 'production' ? '/WeddingForm/' : '/', //發佈對應路徑
 };
