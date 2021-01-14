b032ec49f3a07694ded0151e9ca2ae612389421d
hmlu36 <hmlu36@gmail.com>
2021-01-14 09:39:39 +0800

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 const routerHistory = createWebHistory();
 
-const routes = [
-    {
-        path: "/",
-        name: "RegiestForm",
-        component: () => import("../components/RegiestForm.vue"),
-    },
-    {
-        path: "/carousel",
-        name: "Carousel",
-        component: () => import("../components/Carousel.vue"),
-    },
-];
-
 export default createRouter({
     history: createWebHistory(),
-    routes,
+    routes: [
+        {
+            path: "/",
+            name: "RegiestForm",
+            component: () => import("../components/RegiestForm.vue"),
+        },
+        {
+            path: "/carousel",
+            name: "Carousel",
+            component: () => import("../components/Carousel.vue"),
+        },
+    ],
 });
